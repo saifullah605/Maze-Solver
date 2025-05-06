cece671b6471851fab224b03e50a3fbbdbed2660
Saif Siddiqui <saifullah.siddiqui04@gmail.com>
2025-05-06 16:02:50 -0400

    added window

---
+++ window.py
@@ -0,0 +1,37 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title = "Maze Solver"
+        self.canvas = Canvas(self.__root, bg="white", height = height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+
+    def wait_for_close(self):
+
+        self.running = True
+
+        while(self.running):
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+
+
+
+
+
+
+        
+
+        
