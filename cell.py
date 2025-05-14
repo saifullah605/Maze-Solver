49a623c7812df5a73b72bfb2132dbc8c1a03317d
Saif Siddiqui <saifullah.siddiqui04@gmail.com>
2025-05-13 21:26:29 -0400

    added draw line

--- cell.py
+++ cell.py
@@ -20,6 +20,10 @@ class Cell():
         self.__y2 = y2 
 
         self.__win = win 
+    
+    def get_center_point(self):
+        return Point((self.__x1+self.__x2)/2, (self.__y1 + self.__y2)/2)
+
        
 
 
@@ -42,6 +46,34 @@ class Cell():
         if self.has_top_wall:
             line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
             self.__win.draw_line(line)
+        
+
+    
+    def draw_move(self, to_cell, undo = False):
+        color = "red"
+
+        if undo:
+            color = "gray"
+        
+        line = Line(self.get_center_point(), to_cell.get_center_point())
+        self.__win.draw_line(line, color)
+
+
+        
+
+        
+
+        
+
+
+        
+
+
+
+
+
+    
+
     
 
 
--- main.py
+++ main.py
@@ -6,10 +6,13 @@ def main():
 
 
     cell = Cell(250,250,300,300,win)
+    cell2 = Cell(300,250,350,300,win)
     
 
     cell.draw()
+    cell2.draw()
 
+    cell.draw_move(cell2)
 
     win.wait_for_close()
 
