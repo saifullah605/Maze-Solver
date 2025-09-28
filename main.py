18080b1d5a5cf5059365fb1ce0b1e21642b34e52
saifullah605 <saifullah.siddiqui04@gmail.com>
2025-09-28 00:38:33 -0400

    added button to redraw

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 from window import Window 
 from maze import Maze
 
-def main():
-    win = Window(800,600)
-
-    maze = Maze(100,50,10,10,50,50,win)
+def redraw_and_solve(win):
+    maze = Maze(100, 50, 10, 10, 50, 50, win)
     maze.solve()
-    
- 
 
-
-
-
-
-
-
-
- 
-
+def main():
+    win = Window(800, 600, redraw_and_solve_callback=redraw_and_solve)
+    redraw_and_solve(win)  
     win.wait_for_close()
-
 
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -1,16 +1,29 @@
-from tkinter import Tk, BOTH, Canvas
+from tkinter import Tk, BOTH, Canvas, Button
 from line import Line
 
 
 class Window():
-    def __init__(self, width, height):
+    def __init__(self, width, height, redraw_and_solve_callback=None):
         self.__root = Tk()
         self.__root.title = "Maze Solver"
         self.canvas = Canvas(self.__root, bg="white", height = height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
 
+         # Add Redraw & Solve button
+        self.redraw_button = Button(self.__root, text="Redraw & Solve", command=self._on_redraw_and_solve)
+        self.redraw_button.pack()
+
+        self.redraw_and_solve_callback = redraw_and_solve_callback
+
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+
+
+    def _on_redraw_and_solve(self):
+        self.canvas.delete("all")  
+        if self.redraw_and_solve_callback:
+            self.redraw_and_solve_callback(self)  
 
     
     def redraw(self):
